3497ef66d12fe9413743fc0c36814c9a437b974f
Owner <129130014+hysteria222@users.noreply.github.com>
2024-09-22 19:07:21 -0500

    create new post check, looking over, maybe final commit

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,74 +1,86 @@
 const express = require('express');
 
-const { validateUserId,
-        validateUser,
-        validatePost,
+const {
+  validateUserId,
+  validateUser,
+  validatePost,
 } = require('../middleware/middleware');
 
-const User = require('./users-model')
-const Post = require('../posts/posts-model')
+const User = require('./users-model');
+const Post = require('../posts/posts-model');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
   User.get()
-  .then(users => {
-    res.json(users)
-  })
-  .catch(next)
+    .then((users) => {
+      res.json(users);
+    })
+    .catch(next);
 });
 
 router.get('/:id', validateUserId, (req, res) => {
-  res.json(req.user)
+  res.json(req.user);
 });
 
 router.post('/', validateUser, (req, res, next) => {
   User.insert({ name: req.name })
-  .then(newUser => {
-    res.status(201).json(newUser)
-  })
-  .catch(next)
+    .then((newUser) => {
+      res.status(201).json(newUser);
+    })
+    .catch(next);
 });
 
 router.put('/:id', validateUserId, validateUser, (req, res, next) => {
   User.update(req.params.id, { name: req.name })
-  .then(() => {
-    return User.getById(req.params.id)
-  })
-  .catch(next)
+    .then(() => {
+      return User.getById(req.params.id);
+    })
+    .catch(next);
 });
 
 router.delete('/:id', validateUserId, async (req, res, next) => {
   try {
-    await User.remove(req.params.id)
-    res.json(req.user)
+    await User.remove(req.params.id);
+    res.json(req.user);
   } catch (err) {
-    next(err)
+    next(err);
   }
 });
 
 router.get('/:id/posts', validateUserId, async (req, res, next) => {
   try {
-    const result = await User.getUserPosts(req.params.id)
-    res.json(result)
+    const result = await User.getUserPosts(req.params.id);
+    res.json(result);
   } catch (err) {
-    next(err)
+    next(err);
   }
 });
 
-router.post('/:id/posts', validateUserId, validatePost,(req, res) => {
-  // RETURN THE NEWLY CREATED USER POST
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
-  console.log(req.user, req.text)
-});
+router.post(
+  '/:id/posts',
+  validateUserId,
+  validatePost,
+  async (req, res, next) => {
+    try {
+      const result = Post.insert({
+        user_id: req.params.id,
+        text: req.text,
+      });
+      res.status(201).json(result);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
-router.use((err, req, res, next) => { //eslint-disable-line
+router.use((err, req, res, next) => {
+  //eslint-disable-line
   res.status(err.status || 500).json({
-    customMessage: 'womp x2', 
+    customMessage: 'womp x2',
     message: err.message,
     stack: err.stack,
-  })
-})
+  });
+});
 
 module.exports = router;
